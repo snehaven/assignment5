612fb06a45a3ba1a4ebf3860c9b09f1d6aa30156
snehaven <65678267+snehaven@users.noreply.github.com>
2022-02-27 14:52:00 -0600

    Add files via upload

---
+++ Work.py
@@ -0,0 +1,67 @@
+import sys
+import time
+
+
+# Input: v an integer representing the minimum lines of code and
+#        k an integer representing the productivity factor
+# Output: computes the sum of the series (v + v // k + v // k**2 + ...)
+#         returns the sum of the series
+def sum_series (v, k):
+    exp = 0
+    total = 0
+    term = v
+    while term > 0 :
+        term = v // (k ** exp)
+        total += term
+        exp += 1
+    return total
+
+
+# Input: n an integer representing the total number of lines of code
+#        k an integer representing the productivity factor
+# Output: returns v the minimum lines of code to write using linear search
+def linear_search (n, k):
+    for i in range(1, n + 1):
+        if sum_series(i, k) >= n:
+            return i
+    return i
+
+
+# Input: n an integer representing the total number of lines of code
+#        k an integer representing the productivity factor
+# Output: returns v the minimum lines of code to write using binary search
+def binary_search (n, k):
+    return 2
+
+
+
+def main():
+  # read number of cases
+  line = sys.stdin.readline()
+  line = line.strip()
+  num_cases = int (line)
+
+  for i in range (num_cases):
+    line = sys.stdin.readline()
+    line = line.strip()
+    inp =  line.split()
+    n = int(inp[0])
+    k = int(inp[1])
+
+    start = time.time()
+    print("Binary Search: " + str(binary_search(n, k)))
+    finish = time.time()
+    print("Time: " + str(finish - start))
+
+    print()
+
+    start = time.time()
+    print("Linear Search: " + str(linear_search(n, k)))
+    finish = time.time()
+    print("Time: " + str(finish - start))
+
+    print()
+    print()
+
+if __name__ == "__main__":
+  main()
